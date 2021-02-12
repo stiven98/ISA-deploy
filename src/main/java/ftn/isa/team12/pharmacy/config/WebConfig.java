e8c72d5616c48c3a90be95cc8e8314ef82c3407b
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2021-02-12 21:33:32 +0100

    :rocket: One last time

--- src/main/java/ftn/isa/team12/pharmacy/config/WebConfig.java
+++ src/main/java/ftn/isa/team12/pharmacy/config/WebConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//
+//    }
 }
